368a0b6151ca5532df40d6f28d7cd3f537a6600d
Nadav Gonen <118753575+Nadavgo8@users.noreply.github.com>
2023-05-28 20:56:38 +0300

    Update Ass6Game.java

--- src/Ass6Game.java
+++ src/Ass6Game.java
@@ -1,15 +1,11 @@
-// 209702745 Nadav Gonen
+
 
 import biuoop.GUI;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @author Nadav Gonen nadav.gonen1@live.biu.ac.il
- * @version 1.0
- * @since 2022-09-03
- */
+
 public class Ass6Game {
     /**
      * The main function.
